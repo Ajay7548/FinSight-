c3a369d8f64b164ed9cb6ca6fd67bc3988e0794d
Ajay Mourya <mouryaajay7548@gmail.com>
2026-06-28 01:52:05 +0530

    Hero image updated to use banner.jpeg instead of banner4.webp.

--- components/hero.jsx
+++ components/hero.jsx
@@ -47,7 +47,7 @@ const HeroSection = () => {
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src="/banner4.webp"
+              src="/banner.jpeg"
               // src="/banner2.png"
               width={1280}
               height={720}
